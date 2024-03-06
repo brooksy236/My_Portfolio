a746520c94e39c304918b41f6e369908a6a88492
brooksy236 <david.brooks1980@gmail.com>
2024-03-06 00:07:01 +0000

    updated links

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,8 @@ function About() {
     return <>
         <ul>
         <li><a href="https://github.com/brooksy236">GitHub</a></li>
-        <li><a href="linkedn.com/in/david-brooks-b18820122/">LinkedIn</a></li>
-        <li><a href="codepen.io/brooksy236/pens/">Codepen</a></li>
+        <li><a href="https://linkedin.com/in/david-brooks-b18820122/">LinkedIn</a></li>
+        <li><a href="https://codepen.io/brooksy236/pens/">Codepen</a></li>
 
         </ul>
     </>
